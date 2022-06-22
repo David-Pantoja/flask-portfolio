c65941b3a2720463337bfa94f742dc8c7148108a
David <49932595+David-Pantoja@users.noreply.github.com>
2022-06-21 22:01:27 -0700

    Created Service

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-tmux kill-server
-cd flask-portfolio
+cd /root/flask-portfolio
 git fetch && git reset origin/main --hard
 python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
-tmux new -s flask-portfolio -d
-tmux send-keys -t flask-portfolio "source python3-virtualenv/bin/activate" C-m
-tmux send-keys -t flask-portfolio "flask run --host=0.0.0.0" C-m
+systemctl daemon-reload
+systemctl restart myportfolio
